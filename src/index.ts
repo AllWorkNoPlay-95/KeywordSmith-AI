6e5629ef8d25e4ff5f815900b23656875ff0af6a
Samuele Mancuso <s.mancuso95@gmail.com>
2025-09-18 11:10:26 +0200

    Add product fetching and Ollama interface integration
    Introduced product fetching alongside categories in the main function and added a new Ollama interface for chat interactions. This update allows logging of product details and provides utility functions for interacting with the Ollama API.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import fetchCategories from "./fetch/categories";
-import {CATEGORIES_TARGET_KEY} from "./config";
+import {CATEGORIES_TARGET_KEY, PRODUCTS_TARGET_KEY} from "./config";
 import {logDebug} from "./cli/styles";
+import fetchProducts from "./fetch/products";
 
 async function main(): Promise<void> {
     const categories = await fetchCategories();
     categories.forEach((category: any) => {
         logDebug(category[CATEGORIES_TARGET_KEY]);
     });
+
+    const products = await fetchProducts();
+    products.forEach((product: any) => {
+        logDebug(product[PRODUCTS_TARGET_KEY]);
+    })
 }
 
 main().then();
---
+++ src/interfaces/ollama.ts
@@ -0,0 +1,36 @@
+import ollama from 'ollama';
+import {MODEL} from "../config";
+
+export async function test() {
+    const response = await ollama.chat({
+        model: MODEL,
+        messages: [
+            {
+                role: 'user',
+                content: "Hello World! Reply with 3 words."
+            }
+        ]
+    });
+
+    if (response.message.content) {
+        return response;
+    } else
+        throw new Error("No response from Ollama");
+}
+
+export async function prompt(p: string) {
+    const response = await ollama.chat({
+        model: MODEL,
+        messages: [
+            {
+                role: 'user',
+                content: p
+            }
+        ]
+    });
+
+    if (response.message.content) {
+        return response.message.content;
+    } else
+        throw new Error("No response from Ollama");
+}
