52319e5403a64e294f78ef0edb41d87a374be3cf
Samuele Mancuso <s.mancuso95@gmail.com>
2026-04-13 08:57:56 +0200

    Refactor sendProducts to remove error logging and streamline product sending process

--- src/bin/sendProducts.ts
+++ src/bin/sendProducts.ts
@@ -1,5 +1,13 @@
-import {logError, logOk} from "../cli/styles";
+import {logOk} from "../cli/styles";
+import {getAllIdsDb, readFromDb} from "../interfaces/sqlite";
+import {Payload} from "../types/Payload";
+import {sendGeneratedDescriptions} from "../send/generatedDescriptions";
 
 (async () => {
-    logError("Not ready yet!");
+    const ids = getAllIdsDb("product");
+    for (const id of ids) {
+        const tc = readFromDb(parseInt(String(id)), "product") as Payload;
+        await sendGeneratedDescriptions(tc);
+        logOk(`${tc.name} sent!`);
+    }
 })().then(() => logOk("Products sent!"));
